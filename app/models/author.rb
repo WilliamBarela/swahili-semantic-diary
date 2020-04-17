18a4ef5ba00ad6a791225eaffd63c0e06960bb15
William Barela <william.barela.dev@gmail.com>
2020-04-17 01:08:36 -0500

    add basic validations for new registration of authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,15 @@
 class Author < ApplicationRecord
   has_secure_password
+
+  validates :first_name, :last_name, :email,
+    presence: true
+
+  validates :email,
+    uniqueness: true
+
+  validate :valid_password?
+
+  def valid_password?
+    errors.add(:password, "Password must be between 8 and 32 characters.") unless self.password.length.between?(8,32)
+  end
 end
