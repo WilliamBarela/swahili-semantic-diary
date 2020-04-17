53459d6fa2f065ec2c49f4a9962238ae81f364d3
William Barela <william.barela.dev@gmail.com>
2020-04-16 19:00:31 -0500

    generate authors model with has_secure_password

---
+++ db/migrate/20200416233642_create_authors.rb
@@ -0,0 +1,13 @@
+class CreateAuthors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :authors do |t|
+      t.string :last_name
+      t.string :first_name
+      t.string :email
+      t.string :password_digest
+      t.boolean :admin?
+
+      t.timestamps
+    end
+  end
+end
