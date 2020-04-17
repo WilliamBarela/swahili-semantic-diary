4e71f1bae261248d4d007eabaa47bd18774fc27c
William Barela <william.barela.dev@gmail.com>
2020-04-16 23:42:10 -0500

    add routes for registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,7 @@
 class RegistrationsController < ApplicationController
+#             Prefix         Verb         URI Pattern                      Controller#Action
+#         registrations      POST         /registrations(.:format)         registrations#create
+#      new_registration      GET          /registrations/new(.:format)     registrations#new
+#          registration      DELETE       /registrations/:id(.:format)     registrations#destroy
+
 end
