48058e2df3703a9f3f9f6090acf9bf4922da75ec
William Barela <william.barela.dev@gmail.com>
2020-04-16 23:47:22 -0500

    add sessions routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,7 @@
 class SessionsController < ApplicationController
+#         Prefix    Verb        URI Pattern                   Controller#Action
+#        sessions   POST        /sessions(.:format)           sessions#create
+#     new_session   GET         /sessions/new(.:format)       sessions#new
+#         session   DELETE      /sessions/:id(.:format)       sessions#destroy
+
 end
