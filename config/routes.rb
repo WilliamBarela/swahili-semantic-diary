c08ec334aafb1b63ea02f5995b9962a58370fd79
William Barela <william.barela.dev@gmail.com>
2020-04-18 14:19:28 -0500

    rename sign-up and delete-account routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :registrations, only: [:new, :create, :destroy]
+  get 'sign-up', to: 'registrations#new'
+  post 'sign-up', to: 'registrations#create'
+  delete 'delete-account/:author_id', to: 'registrations#destroy'
+
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :authors, only: [:show, :edit] do
