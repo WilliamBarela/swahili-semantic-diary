5c150c2948287e1ba1fef93fe9c23dd8bd216445
William Barela <william.barela.dev@gmail.com>
2020-04-18 22:51:14 -0500

    add logged_in? helper to check if there is a current author defined

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_author
+  helper_method :current_author, :logged_in?
 
   private
 
   def current_author
     @current_author ||= Author.find_by(id: session[:author_id])
+  end
+
+  def logged_in?
+    !!current_author
   end
 end
