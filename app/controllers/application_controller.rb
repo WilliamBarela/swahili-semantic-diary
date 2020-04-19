f4c97f6cdcf43a063f2e55b2c5823c38eeb621d2
William Barela <william.barela.dev@gmail.com>
2020-04-19 16:11:11 -0500

    improve SoC for is_admin? by moving redirect to redirect_if_not_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,10 +16,10 @@ class ApplicationController < ActionController::Base
   end
 
   def is_admin?
-    if !logged_in?
-      redirect_to login_path
-    else
-      redirect_to author_stories_path(current_author) unless current_author.admin
-    end
+    current_author.admin if current_author
+  end
+
+  def redirect_if_not_admin
+    redirect_to author_stories_path(current_author) unless is_admin?
   end
 end
