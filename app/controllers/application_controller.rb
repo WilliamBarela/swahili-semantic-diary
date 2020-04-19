405e318b51752330466b6dd7b976bdaf7e7f7d15
William Barela <william.barela.dev@gmail.com>
2020-04-19 17:34:09 -0500

    add helper method set_author_if_admin()

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_author, :logged_in?, :authenticate_author!, :is_admin?, :redirect_if_not_admin
+  helper_method :current_author, :logged_in?, :authenticate_author!, :is_admin?, :redirect_if_not_admin, :set_author_if_admin
 
   private
 
@@ -21,5 +21,13 @@ class ApplicationController < ActionController::Base
 
   def redirect_if_not_admin
     redirect_to author_stories_path(current_author) unless is_admin?
+  end
+
+  def set_author_if_admin(author_id)
+    if is_admin?
+      author = Author.find_by_id!(author_id)
+    else
+      author = current_author
+    end
   end
 end
