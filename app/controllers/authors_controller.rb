372cbcf76c80f0c076f127d229007521eb39aa9f
William Barela <william.barela.dev@gmail.com>
2020-04-19 12:30:34 -0500

    add before_action is_admin? for author index method

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,12 +1,16 @@
 class AuthorsController < ApplicationController
-#      Prefix      Verb         URI Pattern                   Controller#Action
-#      authors     GET          /authors(.:format)               authors#index
-#                  POST         /authors(.:format)               authors#create
-#   new_author     GET          /authors/new(.:format)           authors#new
-#  edit_author     GET          /authors/:id/edit(.:format)      authors#edit
-#       author     GET          /authors/:id(.:format)           authors#show
-#                  PATCH        /authors/:id(.:format)           authors#update
-#                  PUT          /authors/:id(.:format)           authors#update
-#                  DELETE       /authors/:id(.:format)           authors#destroy
-# 
+  #      Prefix      Verb         URI Pattern                   Controller#Action
+  #      authors     GET          /authors(.:format)               authors#index
+  #                  POST         /authors(.:format)               authors#create
+  #   new_author     GET          /authors/new(.:format)           authors#new
+  #  edit_author     GET          /authors/:id/edit(.:format)      authors#edit
+  #       author     GET          /authors/:id(.:format)           authors#show
+  #                  PATCH        /authors/:id(.:format)           authors#update
+  #                  PUT          /authors/:id(.:format)           authors#update
+  #                  DELETE       /authors/:id(.:format)           authors#destroy
+
+  before_action :is_admin?, only: [:index]
+
+  def index
+  end
 end
