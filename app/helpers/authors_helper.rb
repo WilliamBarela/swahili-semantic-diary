baf76cc9151b7ce971f252630b748337545e8a78
William Barela <william.barela.dev@gmail.com>
2020-04-19 16:09:17 -0500

    add possessive_display_name helper for names ending in 's'

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -2,4 +2,9 @@ module AuthorsHelper
   def display_name(author)
     "#{author.first_name} #{author.last_name}"
   end
+
+  def possessive_display_name(author)
+    name = display_name(author)
+    name[-1] == "s" ? "#{name}'" : "#{name}'s"
+  end
 end
