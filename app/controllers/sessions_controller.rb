aa0ecd5661b16cc1d1ce9ce343da4f15bf3e7100
William Barela <william.barela.dev@gmail.com>
2020-04-18 21:45:18 -0500

    create new method for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 #         session   DELETE      /sessions/:id(.:format)       sessions#destroy
 
   def new
+    @author = Author.new
   end
 
   def create
