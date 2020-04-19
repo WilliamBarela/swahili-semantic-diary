33eb28c0d3d35cb53af907f7d72b8195844f156c
William Barela <william.barela.dev@gmail.com>
2020-04-18 22:52:30 -0500

    add custom routes to session methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: 'registrations#new'
+
   get 'sign-up', to: 'registrations#new'
   post 'sign-up', to: 'registrations#create'
   delete 'delete-account/:author_id', to: 'registrations#destroy'
 
-  resources :sessions, only: [:new, :create, :destroy]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
+  # resources :sessions, only: [:new, :create, :destroy]
 
   resources :authors, only: [:show, :edit] do
     resources :stories
