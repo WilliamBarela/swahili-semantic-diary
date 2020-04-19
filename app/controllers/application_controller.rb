2e24a61525c05a52c5b7cbd1bb872c4934860a73
William Barela <william.barela.dev@gmail.com>
2020-04-18 20:39:01 -0500

    add current_author helper method to find / set current_author

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_author
+
+  private
+
+  def current_author
+    @current_author ||= Author.find_by(id: session[:author_id])
+  end
 end
