412f0f2514998b40c499f8779196d3bb9c68a7c3
William Barela <william.barela.dev@gmail.com>
2020-04-18 23:17:33 -0500

    change redirect to author_stories_path in preparation of using a partial for a new author story

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
     @author = Author.find_by(email: params[:author][:email])
     if @author && @author.authenticate(params[:author][:password])
       session[:author_id] = @author.id
-      redirect_to new_author_story_path(@author)
+      redirect_to author_stories_path(@author)
     else
       render :new
     end
