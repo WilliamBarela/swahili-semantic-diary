0116db2d388fb67bd72c8e5894024d2ad3038201
William Barela <william.barela.dev@gmail.com>
2020-04-20 23:57:21 -0500

    add valid title length validation

--- app/models/story.rb
+++ app/models/story.rb
@@ -6,4 +6,10 @@ class Story < ApplicationRecord
 
   validates :story_title,
     uniqueness: true
+
+  validate :valid_title_length?
+
+  def valid_title_length?
+    errors.add(:story_title, "is currently #{self.story_title.length} characters long. It must be between 3 and 60 characters") unless self.story_title.nil? or self.story_title.length.between?(3,60)
+  end
 end
