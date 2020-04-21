cf6d521170520d91c4b050655d3957b645ec4fd4
William Barela <william.barela.dev@gmail.com>
2020-04-20 23:52:31 -0500

    remove rundent 'Password' word in :password error

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ApplicationRecord
   has_secure_password
+  has_many :stories 
 
   validates :first_name, :last_name, :email,
     presence: true
@@ -10,6 +11,6 @@ class Author < ApplicationRecord
   validate :valid_password?
 
   def valid_password?
-    errors.add(:password, "Password must be between 8 and 32 characters.") unless self.password.nil? or self.password.length.between?(8,32)
+    errors.add(:password, "must be between 8 and 32 characters.") unless self.password.nil? or self.password.length.between?(8,32)
   end
 end
