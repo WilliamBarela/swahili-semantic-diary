389421ebc03dd72904ca950e8c97d91e1807628d
William Barela <william.barela.dev@gmail.com>
2020-04-25 01:12:59 -0500

    make working oauth session creation...this is ugly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,27 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @author = Author.find_by(email: params[:author][:email]) || Author.new
-    if non_nil_author_authenticates(@author, params)
-      session[:author_id] = @author.id
-      route_created_session(@author)
+    if auth_hash = request.env["omniauth.auth"]
+      oauth_username = request.env["omniauth.auth"]["extra"]["raw_info"]["login"]
+
+      if @author = Author.find_by(:username => oauth_username)
+        session[:author_id] = @author.id
+        route_created_session(@author)
+      else
+        @author = Author.new(:username => oauth_username, :password => SecureRandom.hex)
+        @author.save!(:validate => false)
+        session[:author_id] = @author.id
+        redirect_to edit_author_path(@author)
+      end
     else
-      flash[:alert] = "Email or password is incorrect"
-      redirect_to login_path
+      @author = Author.find_by(email: params[:author][:email]) || Author.new
+      if non_nil_author_authenticates(@author, params)
+        session[:author_id] = @author.id
+        route_created_session(@author)
+      else
+        flash[:alert] = "Email or password is incorrect"
+        redirect_to login_path
+      end
     end
   end
 
