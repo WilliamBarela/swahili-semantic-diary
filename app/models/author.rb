4d0922be7cf31cabbe45c5c44aaf810247697730
William Barela <william.barela.dev@gmail.com>
2020-04-25 02:18:51 -0500

    add class method find_or_create_by_omniauth to return author

--- app/models/author.rb
+++ app/models/author.rb
@@ -13,4 +13,15 @@ class Author < ApplicationRecord
   def valid_password?
     errors.add(:password, "must be between 8 and 32 characters.") unless self.password.nil? or self.password.length.between?(8,32)
   end
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    oauth_username = auth_hash["extra"]["raw_info"]["login"]
+
+    unless author = Author.find_by(:username => oauth_username)
+      author = Author.new(:username => oauth_username, :password => SecureRandom.hex)
+      author.save!(:validate => false)
+    end
+
+    return author
+  end
 end
