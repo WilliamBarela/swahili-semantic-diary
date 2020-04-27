1d6eaf987a21350ab1e5653d7eef7a25fe8c6494
William Barela <william.barela.dev@gmail.com>
2020-04-27 02:25:18 -0500

    add rails generated gloss model

---
+++ app/models/gloss.rb
@@ -0,0 +1,4 @@
+class Gloss < ApplicationRecord
+  belongs_to :lemma
+  belongs_to :story
+end
---
+++ db/migrate/20200427072327_create_glosses.rb
@@ -0,0 +1,11 @@
+class CreateGlosses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :glosses do |t|
+      t.string :gloss
+      t.belongs_to :lemma, index: true, null: false, foreign_key: true
+      t.belongs_to :story, index: true, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
