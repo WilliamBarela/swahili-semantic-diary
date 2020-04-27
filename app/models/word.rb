063eafaee32a77ce2d3a5b5d6538d757e5b6d2d7
William Barela <william.barela.dev@gmail.com>
2020-04-27 15:57:03 -0500

    add accepts_nested_attributes_for :glosses

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,15 @@
 class Word < ApplicationRecord
   has_many :glosses
   has_many :stories, through: :glosses
+
+  accepts_nested_attributes_for :glosses
+
+  # FIXME: this is not right
+  # def gloss_attributes=(gloss_attributes)
+  #   # raise gloss_attributes.inspect
+  #   gloss_attributes.values.each do |gloss_attribute|
+  #     gloss = Gloss.find_or_create_by(gloss_attribute)
+  #     self.glosses.build(gloss: gloss)
+  #   end
+  # end
 end
