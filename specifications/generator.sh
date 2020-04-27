f5a1fb5343fc63c077dc3ea93f509819da9ea47b
William Barela <william.barela.dev@gmail.com>
2020-04-27 02:21:20 -0500

    change specifications due to StoryLemma being reworked as Gloss

--- specifications/generator.sh
+++ specifications/generator.sh
@@ -3,13 +3,13 @@
 rails g model Author last_name:string first_name:string email:string password_digest:string admin?:boolean
 
 # generate Lemma model:
-rails g model Lemma lemma:string lexical_category:string class:string irregularity:string origin:string
+rails g model Lemma lemma:string lexical_category:string class:string notes:string origin:string
 
 # generate Story model:
 rails g model Story story_title:string story:text author:references{index}
 
 # generate StoryLemma model
-rails g model StoryLemma gloss:string lemma:belongs_to{index} story:belongs_to{index}
+rails g model Gloss gloss:string lemma:belongs_to{index} story:belongs_to{index}
 
 ## Controllers ##
 # generate controller for Authors
@@ -21,5 +21,5 @@ rails g controller Lemmas
 # generate controller for Stories
 rails g controller Stories
 
-# generate controller for StoryLemmas
-rails g controller StoryLemmas
+# generate controller for Glosses
+rails g controller Glosses
