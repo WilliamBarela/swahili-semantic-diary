738df17a85507145c724ea63afbcd52ea60112cf
William Barela <william.barela.dev@gmail.com>
2020-04-27 15:58:13 -0500

    initial stab at getting word create to work

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,19 @@
 class WordsController < ApplicationController
+  def new
+    @word = current_story.words.build
+  end
+
+  def create
+    @word = current_story.words.build(word_params)
+    if @word.save
+      redirect_to author_stories_path(current_author)
+    else
+      raise "word not saved :(".inspect
+    end
+  end
+
+  private
+    def word_params
+      params.require(:word).permit(:lemma, :lexical_category, :lemma_class, :notes, :origin, gloss_attributes: [:gloss])
+    end
 end
