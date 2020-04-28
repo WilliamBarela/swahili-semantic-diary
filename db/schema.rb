137cfbee2125096376e6500d2dc20019b34ab1fb
William Barela <william.barela.dev@gmail.com>
2020-04-28 14:08:13 -0500

    update schema to include author reference in words

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_170711) do
+ActiveRecord::Schema.define(version: 2020_04_28_182512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +53,12 @@ ActiveRecord::Schema.define(version: 2020_04_27_170711) do
     t.string "origin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_words_on_author_id"
   end
 
   add_foreign_key "glosses", "stories"
   add_foreign_key "glosses", "words"
   add_foreign_key "stories", "authors"
+  add_foreign_key "words", "authors"
 end
